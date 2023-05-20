8918c5bf8659100771d0bd8e81bf2c931dab30b9
hardiksuthar74 <76840141+hardiksuthar74@users.noreply.github.com>
2023-05-20 14:13:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  console.log(btnNavEl);
+  console.log(headerEl);
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// Assignment
+const validateForm = () => {
+  let name = document.forms[0].elements[0].value;
+  let email = document.forms[0].elements[1].value;
+
+  if (name === "" || email === "") {
+    alert("Please fill in all fields.");
+    return false;
+  }
+
+  return true;
+};
